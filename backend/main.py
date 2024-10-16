29a2ce783d8905b776d3fb27615f5f5ce41ba79d
Dinal Fernando <56021139+Dinal-Fernando@users.noreply.github.com>
2024-10-16 13:54:14 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -12,13 +12,3 @@ class Item(BaseModel):
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.post("/items/")
-def create_item(item: Item):
-    return item
